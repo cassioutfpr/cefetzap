763ae3b854bfab15bd7da3eebc0e89ffdb43f9b3
Cassio Morales <cassioutfpr@gmail.com>
2020-03-18 16:27:27 -0300

    First server commit.

---
+++ server/server.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(f"hostname{socket.gethostname()}")
+s.bind(("127.0.0.1", 80))
+s.listen(5)
+
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+    clientsocket.send(bytes("Hey there!!!","utf-8"))
